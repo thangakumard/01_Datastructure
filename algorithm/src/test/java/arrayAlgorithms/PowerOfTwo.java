84f9c65ed8d7ef8576539526d01ff3109070cf5b
Thangakumar <dthangakumar@gmail.com>
2016-11-02 15:54:10 -0700

    PowerOfTwo check

---
+++ algorithm/src/test/java/arrayAlgorithms/PowerOfTwo.java
@@ -0,0 +1,29 @@
+
+package arrayAlgorithms;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PowerOfTwo {
+	
+	@Test
+	public void checkPowerOf2()
+	{
+		int input = 8;
+		
+		if( input < 1)
+			Assert.fail();
+		
+		while(input > 1)
+		{
+			if((input & 1) == 1)// Get the firt bit and check it is 1
+				Assert.fail();
+			
+			input = input >> 1; // // Move the bits for 1 index to the right 
+		}
+		
+		
+		Assert.assertTrue(input == 1);
+	}
+
+}
