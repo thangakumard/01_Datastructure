aa214ce551125dcf61965c08ce67722c13d381d7
Thangakumar <dthangakumar@gmail.com>
2016-11-02 08:35:33 -0700

    HasDuplicate Algorithm

---
+++ algorithm/src/test/java/arrayAlgorithms/HasDuplicate.java
@@ -0,0 +1,25 @@
+package arrayAlgorithms;
+
+import java.util.*;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HasDuplicate {
+	
+	@Test
+	public void CheckDuplicate()
+	{
+		int[] input = { 1,2,3,4,2,1};
+		Boolean hasDuplicate = false;
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		for(int n: input)
+		{
+			if(set.add(n))
+				hasDuplicate = true;
+		}
+		Assert.assertTrue(hasDuplicate);
+	}
+
+}
