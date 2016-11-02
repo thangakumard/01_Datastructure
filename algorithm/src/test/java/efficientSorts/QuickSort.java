7a8a6f354041b7661ed1543340ce1fbb1b07259c
Thangakumar <dthangakumar@gmail.com>
2016-11-02 08:08:49 -0700

    5 Sorting algorithms

---
+++ algorithm/src/test/java/efficientSorts/QuickSort.java
@@ -0,0 +1,52 @@
+package efficientSorts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class QuickSort {
+
+	@Test
+	public void doQuickSort() {
+		int[] input = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 9, 2 };
+		int[] output = { 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9 };
+
+		quickSortRecursive(input, 0, input.length - 1);
+
+		for (int i = 0; i < input.length; i++) {
+			System.out.println("input : " + input[i]);
+		}
+		Assert.assertEquals(input, output, "Merge sort fails!!!");
+	}
+
+	public void quickSortRecursive(int[] input, int left, int right) {
+		int pivotValue = input[left];
+		int initialLeft = left;
+		int initialRight = right;
+		while (left < right) {
+			while (input[right] >= pivotValue && left < right) {
+				right--;
+			}
+			if (left != right) {
+				input[left] = input[right];
+				left++;
+			}
+			while (input[left] <= pivotValue && left < right) {
+				left++;
+			}
+			if (left != right) {
+				input[right] = input[left];
+				right--;
+			}
+
+		}
+
+		input[left] = pivotValue;
+		int pivotIndex = left;
+
+		if (initialLeft < pivotIndex - 1)
+			quickSortRecursive(input, initialLeft, pivotIndex - 1);
+		if (pivotIndex + 1 < initialRight)
+			quickSortRecursive(input, pivotIndex + 1, initialRight);
+	}
+
+}
---
+++ algorithm/src/test/java/simpleSorts/SortingAlgorithms.java
@@ -0,0 +1,113 @@
+package simpleSorts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SortingAlgorithms {
+
+	
+	//Time Complexity
+	// 		Best Case 	 : O(n)
+	//		Average Case : O(n^2)
+	// 		Worst Case 	 : O(n^2)
+	// Space Complexity
+	//      Worst Case : O(1)
+	@Test
+	public void bubbleSort()
+	{
+		int[] input = {9,8,7,6,5,4,3,2,1};
+		int[] output = {1,2,3,4,5,6,7,8,9};
+		int temp = 0;
+		
+		try
+		{
+		for(int i=1; i < input.length; i++)
+		{
+			for(int j=0; j < input.length-i; j++)
+			{
+				if(input[j] > input[j+1])
+				{
+					temp = input[j];
+					input[j] = input[j+1];
+					input[j+1]= temp;
+				}
+			}
+		}
+		}
+		catch(Exception ex)
+		{
+			Assert.fail(ex.getMessage());
+		}		
+		Assert.assertEquals(input, output, "Bubble sort fails !");
+	}
+	
+	//Time Complexity
+		// 		Best Case 	 : O(n)
+		//		Average Case : O(n^2)
+		// 		Worst Case 	 : O(n^2)
+		// Space Complexity
+		//      Worst Case : O(1)
+	@Test
+	public void insertionSort()
+	{
+		int[] input = {9,8,7,6,5,4,3,2,1};
+		int[] output = {1,2,3,4,5,6,7,8,9};
+		int temp = 0;
+		int j = 0;
+		
+		for(int i=1; i<input.length; i++)
+		{
+			j = i;
+			while(j > 0)
+			{
+				if(input[j-1] > input[j])
+				{
+					temp = input[j-1];
+					input[j-1] = input[j];
+					input[j] = temp;
+				}
+				j--;
+			}
+		}		
+		Assert.assertEquals(input, output, "Inserion sort fails !");
+	}
+	
+	//Time Complexity
+		// 		Best Case 	 : O(n^2)
+		//		Average Case : O(n^2)
+		// 		Worst Case 	 : O(n^2)
+		// Space Complexity
+		//      Worst Case : O(log (n))
+	@Test
+	public void selectionSort()
+	{
+		int[] input = {9,8,7,6,5,4,3,2,1};
+		int[] output = {1,2,3,4,5,6,7,8,9};
+		int temp = 0;
+		int minimum = 0;
+		
+		for(int i=0; i < input.length-1; i++)
+		{
+			minimum = i;
+			for(int j=i+1; j <input.length;j++ )
+			{
+				if(input[minimum] > input[j])
+				{
+					minimum = j;
+				}
+			}
+			
+			if(minimum != i)
+			{
+				temp = input[i];
+				input[i] = input[minimum];
+				input[minimum] = temp;
+			}
+		}
+		
+		System.out.println("The first element is:" + input[0]);
+		
+		Assert.assertEquals(input, output, "Selection sort fails !");
+	}
+}
