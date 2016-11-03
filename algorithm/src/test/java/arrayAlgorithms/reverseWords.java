723e399f29ed8ad60fef29140627abf8c7d88de3
Thangakumar <dthangakumar@gmail.com>
2016-11-03 12:49:42 -0700

    reverseWord

---
+++ algorithm/src/test/java/arrayAlgorithms/reverseWords.java
@@ -0,0 +1,47 @@
+package arrayAlgorithms;
+
+import org.testng.annotations.Test;
+
+public class reverseWords {
+	
+	
+	@Test
+	public void getReverseWord()
+	{
+		char[] input = "god is everywhere".toCharArray();
+		int i =0;
+		
+		for(int j=0; j < input.length; j++)
+		{
+			if(input[i] == ' '){
+				reverse(input,i,j-1);
+				i = j+1;
+			}
+			
+			System.out.println(String.valueOf(input));
+
+			reverse(input, i, input.length-1);
+			System.out.println(input.toString());
+
+			System.out.println(String.valueOf(input));
+
+		}
+		System.out.println("rever string is : ");
+
+		System.out.println(String.valueOf(input));
+	}
+
+	
+	private void reverse(char[] s,int left,int right)
+	{
+		char temp;
+		while(left < right)
+		{
+			temp = s[left];
+			s[left++] = s[right];
+			s[right--] = temp;
+		}
+	}
+
+}
+
