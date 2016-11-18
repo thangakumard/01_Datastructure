62be0af93ce9e5ead7ee6a1afa6df3059fca0234
Thangakumar <dthangakumar@gmail.com>
2016-11-17 16:27:00 -0800

    Palindrome

--- algorithm/src/test/java/singlyLinkedList/Node.java
+++ algorithm/src/test/java/singlyLinkedList/Node.java
@@ -4,9 +4,16 @@ public class Node {
 
 	public int value;
 	public Node next = null;
-	
-	public Node(int x){
+
+	public Node(int x) {
 		value = x;
-		next = null; 
+		next = null;
+	}
+
+	public char charValue;
+
+	public Node(char x) {
+		charValue = x;
+		next = null;
 	}
 }
