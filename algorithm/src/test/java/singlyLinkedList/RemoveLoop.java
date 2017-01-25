f143e2e1a976a47081c9906aa84ae9abf827c336
Thangakumar <dthangakumar@gmail.com>
2017-01-25 12:04:50 -0800

    added file RemoveLoop.java

---
+++ algorithm/src/test/java/singlyLinkedList/RemoveLoop.java
@@ -0,0 +1,83 @@
+package singlyLinkedList;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/*Detect and Remove Loop in a Linked List
+Write a function detectAndRemoveLoop() that checks whether a given Linked List contains loop and if loop is present then removes the loop and returns true. 
+And if the list doesn’t contain loop then returns false. Below diagram shows a linked list with a loop. 
+detectAndRemoveLoop() must change the below list to 1->2->3->4->5->NULL.
+
+http://www.geeksforgeeks.org/detect-and-remove-loop-in-a-linked-list/
+*/
+
+public class RemoveLoop {
+	
+	@Test
+	public void testRemoveLoop(){
+		
+		SinglyLinkedList list = new SinglyLinkedList();
+		list.push(new Node(1));
+		list.push(new Node(2));
+		list.push(new Node(3));
+		list.push(new Node(4));
+		list.push(new Node(5));
+		list.push(new Node(6));
+		
+		list.head.next.next.next.next = list.head.next;	
+		
+		findAndDeleteLoop(list.head);
+		Node currentNode = list.head;
+		while(currentNode != null)
+		{
+			System.out.println(currentNode.value);
+			currentNode = currentNode.next;
+		}
+		
+		Assert.assertTrue(true);
+	}
+	
+	
+	public int findAndDeleteLoop(Node node){
+		
+		Node slow = node; Node fast = node;
+		
+		while(slow != null && fast != null && fast.next != null){			
+			slow = slow.next;
+			fast = fast.next.next;		
+			
+			if(slow == fast){
+				removeLoop(slow , node);
+				return 1;
+			}
+		}		
+		return 0;	
+		
+	}	
+	
+	public void removeLoop(Node loop, Node head){
+		
+		Node ptr1 = null, ptr2 = null;
+		
+		ptr1 = head;
+		
+		while(true){
+			
+			ptr2 = loop;
+			while(ptr2.next != loop && ptr2.next != ptr1){
+				ptr2 = ptr2.next;
+			}
+			
+			if(ptr2.next == ptr1){
+				break;
+			}
+			
+			ptr1 = ptr1.next;
+		
+		}
+		
+		ptr2.next = null;
+	}	
+	
+	
+
+}
