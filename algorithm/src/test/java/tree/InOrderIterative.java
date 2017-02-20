f29b0b5ed296e793f392b661c19ab9c0b589394b
Thangakumar <dthangakumar@gmail.com>
2017-02-19 23:08:45 -0800

    Inorder traversal Iterative

---
+++ algorithm/src/test/java/tree/InOrderIterative.java
@@ -0,0 +1,65 @@
+package tree;
+
+import java.util.Stack;
+
+/*
+1) Create an empty stack S.
+2) Initialize current node as root
+3) Push the current node to S and set current = current->left until current is NULL
+4) If current is NULL and stack is not empty then 
+     a) Pop the top item from stack.
+     b) Print the popped item, set current = popped_item->right 
+     c) Go to step 3.
+5) If current is NULL and stack is empty then we are done.
+ */
+public class InOrderIterative {
+
+public void test()
+{
+	 /* creating a binary tree and entering 
+    the nodes */
+   BinaryTree tree = new BinaryTree();
+   tree.root = new Node(1);
+   tree.root.left = new Node(2);
+   tree.root.right = new Node(3);
+   tree.root.left.left = new Node(4);
+   tree.root.left.right = new Node(5);
+   inorder();
+}
+	
+Node root;
+
+void inorder() {
+    if (root == null) {
+        return;
+    }
+    
+    //keep the nodes in the path that are waiting to be visited
+    Stack<Node> stack = new Stack<Node>();
+    Node node = root;
+     
+    //first node to be visited will be the left one
+    while (node != null) {
+        stack.push(node);
+        node = node.left;
+    }
+     
+    // traverse the tree
+    while (stack.size() > 0) {
+       
+        // visit the top node
+        node = stack.pop();
+        System.out.print(node.key + " ");
+        if (node.right != null) {
+            node = node.right;
+             
+            // the next node to be visited is the leftmost
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+    }
+	
+}
+}
