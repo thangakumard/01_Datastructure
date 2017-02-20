96954a0bcbc13cb4e4d75ef4e53926a2bca4dd8c
Thangakumar <dthangakumar@gmail.com>
2017-02-20 08:11:34 -0800

    LevelOrder

---
+++ algorithm/src/test/java/tree/LevelOrderTraversal.java
@@ -0,0 +1,24 @@
+package tree;
+
+/*
+ * printLevelorder(tree)
+1) Create an empty queue q
+2) temp_node = root //start from root
+3) Loop while temp_node is not NULL
+    a) print temp_node->data.
+    b) Enqueue temp_node’s children (first left then right children) to q
+    c) Dequeue a node from q and assign it’s value to temp_node
+ */
+public class LevelOrderTraversal {
+	
+	public void test()
+	{
+		BinaryTree tree = new BinaryTree();
+		tree.root = null;
+		
+	}
+	
+	private void LevelTraversal(){
+		
+	}
+}
