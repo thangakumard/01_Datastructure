2f2bf18b4aed2164437471c251a4ce052676b0fa
Thangakumar <dthangakumar@gmail.com>
2017-02-23 22:38:04 -0800

    New Array related algorithms

--- algorithm/src/test/java/arrayAlgorithms/ArrayIntersection.java
+++ algorithm/src/test/java/arrayAlgorithms/ArrayIntersection.java
@@ -7,9 +7,45 @@ import org.testng.annotations.Test;
 
 public class ArrayIntersection {
 
+	/*Good explanation is in the below article
+	 * http://articles.leetcode.com/here-is-phone-screening-question-from/
+	 * http://stackoverflow.com/questions/15485641/hash-table-vs-sorted-array-which-to-use 
+	 */
+	
+	/****using Array ******/
+	//complexity (O)(m+n)
+	@Test
+	public void findIntersection(){
+		
+		int[] a = {1,2,3,4,5,6,7,8,9};
+		int[] b = {5,6,10,11,12};
+		
+		int m = a.length;
+		int n = b.length;
+		int i= 0; int j = 0;
+		while(i < m && j < n){
+			if(a[i] < b[j]){
+				i++;
+			}
+			else if(a[i] > b[j]){
+				j++;
+			}
+			else
+			{
+				System.out.println(a[i]);
+				i++;
+				j++;
+			}
+		}
+	}
+	
+	
+	
+	/*** using Haseset *******/
+	// complexity (O)(m+n)
 	
 	@Test
-	public void findIntersection() {
+	public void findIntersectionUsingHash() {
 
 		int[] a = { 1, 2, 3, 10, 20, 30, 40 };
 		int[] b = { 2, 3, 4, 5, 6, 7, 8 };
---
+++ algorithm/src/test/java/arrayAlgorithms/UnionOfTwoSortedArray.java
@@ -0,0 +1,48 @@
+package arrayAlgorithms;
+
+import org.testng.annotations.Test;
+
+public class UnionOfTwoSortedArray {
+
+	/*Good explanation is in the below article
+	 * http://articles.leetcode.com/here-is-phone-screening-question-from/
+	 * http://stackoverflow.com/questions/15485641/hash-table-vs-sorted-array-which-to-use 
+	 */
+	@Test
+	public void UnionOfSprtedArray(){
+		
+		int[] a = {1,2,3,4,5,6,7,8,9};
+		int[] b = {1,2,3,4,11,12,13};
+		
+		int m = a.length;
+		int n = b.length;
+		int i=0 , j = 0;
+		
+		while(i < m && j <n){
+			if(a[i] < b[j]){
+				System.out.println(a[i]);
+				i++;
+			}
+			else if(a[i] > b[j]){
+				System.out.println(b[j]);
+				j++;
+			}
+			else
+			{
+				System.out.println(a[i]);
+				i++;
+				j++;
+			}
+		}
+		
+		while(i < m){
+			System.out.println(a[i]);
+			i++;
+		}
+			
+		while(j < n){
+			System.out.println(b[j]);
+			j++;
+		}
+	}
+}
