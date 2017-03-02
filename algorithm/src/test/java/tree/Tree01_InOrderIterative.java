57ce8cccde16898f9a178c9e97d6ae357012001a
Thangakumar <dthangakumar@gmail.com>
2017-03-02 14:47:17 -0800

    Code aligned

--- algorithm/src/test/java/tree/Tree01_InOrderIterative.java
+++ algorithm/src/test/java/tree/Tree01_InOrderIterative.java
@@ -12,54 +12,55 @@ import java.util.Stack;
      c) Go to step 3.
 5) If current is NULL and stack is empty then we are done.
  */
+//http://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
 public class Tree01_InOrderIterative {
 
-public void test()
-{
-	 /* creating a binary tree and entering 
+	public void test()
+	{
+		/* creating a binary tree and entering 
     the nodes */
-   BinaryTree tree = new BinaryTree();
-   tree.root = new Node(1);
-   tree.root.left = new Node(2);
-   tree.root.right = new Node(3);
-   tree.root.left.left = new Node(4);
-   tree.root.left.right = new Node(5);
-   inorder();
-}
-	
-Node root;
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		inorder();
+	}
 
-void inorder() {
-    if (root == null) {
-        return;
-    }
-    
-    //keep the nodes in the path that are waiting to be visited
-    Stack<Node> stack = new Stack<Node>();
-    Node node = root;
-     
-    //first node to be visited will be the left one
-    while (node != null) {
-        stack.push(node);
-        node = node.left;
-    }
-     
-    // traverse the tree
-    while (stack.size() > 0) {
-       
-        // visit the top node
-        node = stack.pop();
-        System.out.print(node.data + " ");
-        if (node.right != null) {
-            node = node.right;
-             
-            // the next node to be visited is the leftmost
-            while (node != null) {
-                stack.push(node);
-                node = node.left;
-            }
-        }
-    }
-	
-}
+	Node root;
+
+	void inorder() {
+		if (root == null) {
+			return;
+		}
+
+		//keep the nodes in the path that are waiting to be visited
+		Stack<Node> stack = new Stack<Node>();
+		Node node = root;
+
+		//first node to be visited will be the left one
+		while (node != null) {
+			stack.push(node);
+			node = node.left;
+		}
+
+		// traverse the tree
+		while (stack.size() > 0) {
+
+			// visit the top node
+			node = stack.pop();
+			System.out.print(node.data + " ");
+			if (node.right != null) {
+				node = node.right;
+
+				// the next node to be visited is the leftmost
+				while (node != null) {
+					stack.push(node);
+					node = node.left;
+				}
+			}
+		}
+
+	}
 }
