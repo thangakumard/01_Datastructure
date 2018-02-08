92e5c5192d2f5a5a6e0d37f71d99937c6f457641
Thangakumar <dthangakumar@gmail.com>
2018-02-07 22:29:56 -0800

    added ArrayDeque

---
+++ algorithm/src/test/java/coreJava/SampleArrayDeque.java
@@ -0,0 +1,29 @@
+package coreJava;
+import java.util.*;
+
+import org.testng.annotations.Test;
+public class SampleArrayDeque {
+
+	/******* ArrayDeque as stack ***********/
+	@Test
+	public void test(){
+		 Deque<String> stack = new ArrayDeque<String>();
+		    stack.push("first");
+		    stack.push("second");
+		    stack.push("Third");
+		    System.out.println("getFirst()" + stack.getFirst());
+		    System.out.println("pop()" + stack.pop());		    
+	}
+	
+	/*******ArrayDeque as a Queue*********/
+	@Test
+	public void whenOffer_addsAtLast() {
+	    Deque<String> queue = new ArrayDeque<String>();
+	    queue.offer("first");
+	    queue.offer("second");
+	    queue.offer("third");
+	  
+	    System.out.println("getLast()" + queue.getLast());
+	    System.out.println("poll()" + queue.poll());	    
+	}	
+}
---
+++ algorithm/src/test/java/coreJava/SampleHashTable.java
@@ -0,0 +1,12 @@
+package coreJava;
+/*
+ * https://docs.oracle.com/javase/7/docs/api/java/util/Hashtable.html
+ * 
+ */
+import java.util.Hashtable;
+import org.testng.annotations.Test;
+@Test
+public class SampleHashTable {
+	Hashtable<String,Integer> number = new Hashtable<String, Integer>();
+	
+}
