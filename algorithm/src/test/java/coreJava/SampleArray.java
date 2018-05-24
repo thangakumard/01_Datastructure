7803ee87fee259ed9a2cf458080134d40388098f
Thangakumar <dthangakumar@gmail.com>
2018-05-24 15:41:39 -0700

    Modified SampleArray.java file

--- algorithm/src/test/java/coreJava/SampleArray.java
+++ algorithm/src/test/java/coreJava/SampleArray.java
@@ -9,25 +9,43 @@ public class SampleArray {
 	@Test
 	public void Test(){
 		
-		  int[] testArray1 = {1, 2, 3, 4, 5};
-	      int testArray2[] = {6, 7, 8, 9, 10};
-	      int[] testArray3 = new int[] {11, 12, 13, 14, 15};
-	      int testArray4[] = new int[] {16, 17, 18, 19, 20};
-	      System.out.println(Arrays.toString(testArray1));
-	      System.out.println(Arrays.toString(testArray2));
-	      System.out.println(Arrays.toString(testArray3));
-	      System.out.println(Arrays.toString(testArray4));
-	      
-	      
-	      int[] testArray = new int[10];
-	      Arrays.fill(testArray, 50);
-	      System.out.println(Arrays.toString(testArray));//[50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
-	      
-		//integer array
-		int arr[] = {1,2,3,4,5};
-		System.out.println(arr.length);
-		System.out.println(arr[0]);
-		
+		 // 1. Initialize
+			int[] testArray1 = new int[5];
+			int[] testArray2 = {1, 2, 3};		
+		 // 2. Get Length
+			System.out.println("The size of testArray1 is: " + testArray1.length);
+		// 3. Access Element
+			System.out.println("The first element is: " + testArray1[0]);
+		// 4. Iterate all Elements
+			System.out.print("[Version 1] The contents of testArray1 are:");
+			for (int i = 0; i < a1.length; ++i) {
+			    System.out.print(" " + testArray1[i]);
+			}
+			System.out.println();
+			System.out.print("[Version 2] The contents of testArray2 are:");
+			for (int item: testArray2) {
+			    System.out.print(" " + item);
+			}
+		System.out.println();
+		// 5. Modify Element
+			a1[0] = 4;
+		// 6. Sort
+			Arrays.sort(a1);		   
+	      	// 7. fill	      
+			int[] testArray3 = new int[10];
+			Arrays.fill(testArray3, 50);
+			System.out.println(Arrays.toString(testArray3));//[50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
+			// To List
+			List list1 = Arrays.asList(testArray3);
+			// printing the list
+			System.out.println("The list is:" + list1);
+		//8. Binaray Search
+			//binarySearch(int[] a, int key)
+			//binarySearch(int[] a, int fromIndex, int toIndex, int key)
+			Arrays.binarySearch(testArray3, 0, 5, 50);
+		//9. Compare 2 array
+			Arrays.equals(testArray2,testArray3);
+		/***** Char Array *******/
 		char input[] = {'a','b','c'};
 		System.out.println(input.length); //length is a variable
 		
