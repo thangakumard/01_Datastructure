50069d30523cf846be26280b7819ffc582a93e68
Thangakumar <dthangakumar@gmail.com>
2018-05-25 09:38:52 -0700

    Sample_2DArray

---
+++ algorithm/src/test/java/coreJava/Sample_2DArray.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+public class Sample_2DArray {
+    private static void printArray(int[][] a) {
+        for (int i = 0; i < a.length; ++i) {
+            System.out.println(a[i]);
+        }
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; a[i] != null && j < a[i].length; ++j) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    @Test
+    public void 2DArraySample(){
+        System.out.println("Example I:");
+        int[][] a = new int[2][5];
+        printArray(a);
+        System.out.println("Example II:");
+        int[][] b = new int[2][];
+        printArray(b);
+        System.out.println("Example III:");
+        b[0] = new int[3];
+        b[1] = new int[5];
+        printArray(b);
+    }
+}
