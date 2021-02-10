d67eca5cccce4d1ad813dc676b37dfd377681737
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2021-02-10 11:18:41 -0800

    Fluent wait

--- algorithm/src/main/java/ui_automation/SampleUIAutomation.java
+++ algorithm/src/main/java/ui_automation/SampleUIAutomation.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class SampleUIAutomation {
 
@@ -26,10 +26,21 @@ public class SampleUIAutomation {
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("http://www.sonos.com");
 		
-		Thread.sleep(1000);
+		 // Waiting 30 seconds for an element to be present on the page, checking
+		   // for its presence once every 5 seconds.
+		   Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+		       .withTimeout(60, TimeUnit.SECONDS)
+		       .pollingEvery(5, TimeUnit.SECONDS)
+		       .ignoring(NoSuchElementException.class);
+
+		   WebElement popup = wait.until(new Function<WebDriver, WebElement>() {
+		     public WebElement apply(WebDriver driver) {
+		       return driver.findElement(By.id("bx-close-inside-1175698"));
+		     }
+		   });
 		
 		
-		WebElement popup = driver.findElement(By.id("bx-close-inside-1175698"));
+		//WebElement popup = driver.findElement(By.id("bx-close-inside-1175698"));
 		if(popup != null) {
 			popup.click();
 		}
@@ -40,10 +51,18 @@ public class SampleUIAutomation {
 		
 		System.out.println("liElements.size() :" + liElements.size());
 		
-		WebElement title = driver.findElement(By.id("beside-n-headline"));
+		//WebElement title = driver.findElement(By.id("beside-n-headline"));
+		WebElement title = driver.findElement(By.xpath("//*[@id='beside-n-headline']"));
+	
 		
 		System.out.println("Color :" +  title.getCssValue("color"));
 		
+		WebElement div = driver.findElement(By.xpath("//div[@id='product-row-homepage-main']"));
 		
+		System.out.println(div.getAttribute("class"));
+		
+		WebElement ul_element = driver.findElement(By.xpath("//div[@id='product-row-homepage-main']/ul[1]"));
+		WebElement li_element = driver.findElement(By.xpath("//div[@id='product-row-homepage-main']/ul[1]/li[1]"));
+
 	}
 }
