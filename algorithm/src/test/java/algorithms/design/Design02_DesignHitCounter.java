e3c183009ae6d5aba7f059a30ec5d885a9a72e67
Thangakumar Dhanasekaran <thanga.dhana@sea.samsung.com>
2021-03-01 13:17:58 -0800

    Leetcode design algorithms

---
+++ algorithm/src/test/java/algorithms/design/Design02_DesignHitCounter.java
@@ -0,0 +1,66 @@
+package algorithms.design;
+
+import java.util.*;
+
+/******
+ * https://leetcode.com/problems/design-hit-counter/
+ * 
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+
+Each function accepts a timestamp parameter (in seconds granularity) and you may assume that calls are being made to the system in chronological order (ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+It is possible that several hits arrive roughly at the same time.
+
+Example:
+
+HitCounter counter = new HitCounter();
+
+// hit at timestamp 1.
+counter.hit(1);
+
+// hit at timestamp 2.
+counter.hit(2);
+
+// hit at timestamp 3.
+counter.hit(3);
+
+// get hits at timestamp 4, should return 3.
+counter.getHits(4);
+
+// hit at timestamp 300.
+counter.hit(300);
+
+// get hits at timestamp 300, should return 4.
+counter.getHits(300);
+
+// get hits at timestamp 301, should return 3.
+counter.getHits(301); 
+Follow up:
+What if the number of hits per second could be very large? Does your design scale?
+ *
+ */
+
+public class Design02_DesignHitCounter {
+	public class HitCounter{
+    Queue<Integer> hitQueue;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        hitQueue = new LinkedList<Integer>();
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        hitQueue.add(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while(!hitQueue.isEmpty() && timestamp - hitQueue.peek() >= 300){
+            hitQueue.poll();
+        }
+        return hitQueue.size();  
+    }
+	}
+}
