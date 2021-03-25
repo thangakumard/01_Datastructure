a205bc96b830cd2dd4ea248b71812ea6a1186b7c
Thangakumar Dhanasekaran <thanga.dhana@sea.samsung.com>
2021-03-24 17:11:30 -0700

    updated few algorithms

--- algorithm/src/test/java/algorithms/tree/binaryTree/BinaryTree09_PushAllNodestoRight.java
+++ algorithm/src/test/java/algorithms/tree/binaryTree/BinaryTree09_PushAllNodestoRight.java
@@ -2,13 +2,39 @@ package algorithms.tree.binaryTree;
 
 import java.util.*;
 
+import org.testng.annotations.Test;
+
+import algorithms.tree.BinaryTree;
 import algorithms.tree.TreeNode;
+import algorithms.tree.BinarySearchTree.BST02_BSTFromSortedArray;
 
 /*
  * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
  * 
  */
 public class BinaryTree09_PushAllNodestoRight {
+	
+	@Test
+	public void test()
+	{
+		/* creating a binary tree and entering 
+    the TreeNodes 
+    				1
+    		2				3
+    	4		5
+    	
+		*/
+		BinaryTree tree = new BinaryTree();
+		tree.root = new TreeNode(1);
+		tree.root.left = new TreeNode(2);
+		tree.root.right = new TreeNode(3);
+		tree.root.left.left = new TreeNode(4);
+		tree.root.left.right = new TreeNode(5);
+		tree.root.left.right.right = new TreeNode(7);
+		
+		
+		flatten(tree.root);
+	}
 
 	public void flatten(TreeNode root) {
 		if (root != null) {
@@ -30,7 +56,7 @@ public class BinaryTree09_PushAllNodestoRight {
 					currentNode.right = stackNodes.peek();
 				}
 
-				currentNode.left = null;
+				currentNode.left = null; /* Important step ***/
 			}
 		}
 	}
