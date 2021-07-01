b605c1b98105cbabd5ec6c3682dc1d498540d0a4
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2021-07-01 10:10:08 -0700

    added palindrome-partitioning

--- algorithm/src/test/java/algorithms/string/palindrome/String12_PalindromicPartitioning.java
+++ algorithm/src/test/java/algorithms/string/palindrome/String12_PalindromicPartitioning.java
@@ -1,5 +1,10 @@
 package algorithms.string.palindrome;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
 /*
  * https://www.educative.io/module/lesson/data-structures-in-java/JP9wy4p5682
  * https://leetcode.com/problems/palindrome-partitioning/
@@ -27,5 +32,46 @@ package algorithms.string.palindrome;
 
  */
 public class String12_PalindromicPartitioning {
+	
+	@Test
+	private void test() {
+		List<List<String>> result = partition("aab");
+		for(List<String> lst: result) {
+			System.out.println(lst.toString());
+		}
+	}
 
+	public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtracking(result, s, new ArrayList<>(), 0);
+        return result;
+            
+    }
+    private void backtracking(List<List<String>> result, String s, List<String> lsttemp, int index){
+        if(index == s.length()){
+            result.add(new ArrayList<>(lsttemp));
+            return;
+        }
+        
+        for(int i=index; i< s.length(); i++){
+            if(isPalindrome(s, index, i)){
+                lsttemp.add(s.substring(index, i+1));
+                System.out.println("Adding :" + s.substring(index, i+1));
+                backtracking(result,s,lsttemp,i+1);
+                System.out.println("Removing :" + lsttemp.get(lsttemp.size()-1));
+                lsttemp.remove(lsttemp.size()-1);
+            }
+        }
+    }
+    
+    private boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
