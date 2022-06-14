c98dedd4be7137bd5dd187a3b6e081fbd106709d
Thangakumar <dthangakumar@gmail.com>
2022-06-14 15:37:09 +0530

    Update BinaryTree20_MinimumDepthBinaryTree.java

--- algorithm/src/test/java/algorithms/tree/binaryTree/BinaryTree20_MinimumDepthBinaryTree.java
+++ algorithm/src/test/java/algorithms/tree/binaryTree/BinaryTree20_MinimumDepthBinaryTree.java
@@ -38,9 +38,9 @@ public class BinaryTree20_MinimumDepthBinaryTree {
     public int minDepth(TreeNode root) {
         if(root == null)
             return 0;
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
         
-        return 1 + Math.max(leftDepth, rightDepth);
+        return 1 + Math.min(leftDepth, rightDepth);
     }
 }
