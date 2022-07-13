862615780726ef077ae265281501605ec43be0be
Thangakumar <dthangakumar@gmail.com>
2022-07-12 22:25:13 -0700

    Update String05_Palindrome_Longest_Substring.java

--- algorithm/src/test/java/algorithms/string/palindrome/String05_Palindrome_Longest_Substring.java
+++ algorithm/src/test/java/algorithms/string/palindrome/String05_Palindrome_Longest_Substring.java
@@ -5,7 +5,18 @@ import org.testng.annotations.Test;
 /***
  * https://leetcode.com/problems/longest-palindromic-substring/
  * 
- */
+ Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+*
+***/
 public class String05_Palindrome_Longest_Substring {
 
 	@Test
