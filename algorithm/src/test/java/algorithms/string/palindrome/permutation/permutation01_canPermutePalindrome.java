8feb8c41847ad9536a9110a1056df054a003ad1f
Thangakumar <dthangakumar@gmail.com>
2022-07-12 22:51:44 -0700

    Create permutation01_canPermutePalindrome.java

---
+++ algorithm/src/test/java/algorithms/string/palindrome/permutation/permutation01_canPermutePalindrome.java
@@ -0,0 +1,36 @@
+package algorithms.string.palindrome.permutation;
+
+import java.util.*;
+
+public class permutation01_canPermutePalindrome {
+
+	/*
+	 * https://leetcode.com/problems/palindrome-permutation/ Given a string,
+	 * determine if a permutation of the string could form a palindrome.
+	 * 
+	 * Example 1:
+	 * Input: "code" Output: false 
+	 * 
+	 * Example 2:
+	 * Input: "aab" Output: true 
+	 * 
+	 * Example 3:
+	 * Input: "carerac" Output: true
+	 * 
+	 * 
+	 * Time complexity O(n) Space complexity O(1)
+	 */
+	public boolean canPermutePalindrome(String s) {
+		Set<Character> set_Input = new HashSet<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			if (set_Input.contains(s.charAt(i))) {
+				set_Input.remove(s.charAt(i));
+			} else {
+				set_Input.add(s.charAt(i));
+			}
+		}
+		if (set_Input.size() < 2)
+			return true;
+		return false;
+	}
+}
