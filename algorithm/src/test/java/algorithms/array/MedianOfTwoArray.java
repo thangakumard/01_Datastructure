d42b9705ba33f3298d14151768b88dc889528a75
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-07-31 16:17:31 -0700

    Added a new solution to MedianOfTwoArray.java

--- algorithm/src/test/java/algorithms/array/MedianOfTwoArray.java
+++ algorithm/src/test/java/algorithms/array/MedianOfTwoArray.java
@@ -9,15 +9,64 @@ public class MedianOfTwoArray {
 		int[] a = { 1, 3, 5, 7, 9 };
 		int[] b = { 2, 4, 6, 8, 10 };
 
-		double median = findMedianSortedArrays(a, b);
+		double median = findMedianSortedArrays_01(a, b);
+		System.out.println(median);
+
+		median = findMedianSortedArrays_02(a, b);
 		System.out.println(median);
 	}
 
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	//Reference : https://www.youtube.com/watch?v=LPFhl65R7ww
+	public double findMedianSortedArrays_01(int[] input1, int[] input2) {
+		//if input1 length is greater than switch them so that input1 is smaller than input2.
+		if (input1.length > input2.length) {
+			return findMedianSortedArrays_01(input2, input1);
+		}
+		int x = input1.length;
+		int y = input2.length;
+
+		int low = 0;
+		int high = x;
+		while (low <= high) {
+			int partitionX = (low + high)/2;
+			int partitionY = (x + y + 1)/2 - partitionX;
+
+			//if partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+			//if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+			int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
+			int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
+
+			int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
+			int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
+
+			if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+				//We have partitioned array at correct place
+				// Now get max of left elements and min of right elements to get the median in case of even length combined array size
+				// or get max of left for odd length combined array size.
+				if ((x + y) % 2 == 0) {
+					return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+				} else {
+					return (double)Math.max(maxLeftX, maxLeftY);
+				}
+			} else if (maxLeftX > minRightY) { //we are too far on right side for partitionX. Go on left side.
+				high = partitionX - 1;
+			} else { //we are too far on left side for partitionX. Go on right side.
+				low = partitionX + 1;
+			}
+		}
+
+		//Only we we can come here is if input arrays were not sorted. Throw in that scenario.
+		throw new IllegalArgumentException();
+	}
+
+
+	public double findMedianSortedArrays_02(int[] nums1, int[] nums2) {
 		int total = nums1.length + nums2.length;
 		if (total % 2 == 0) {
-			return (findKth(total / 2 + 1, nums1, nums2, 0, 0) + findKth(total / 2, nums1, nums2, 0, 0)) / 2.0;
+			//For even length of array find the median of middle 2 elements
+			return (findKth(total / 2, nums1, nums2, 0, 0) + findKth(total / 2 + 1, nums1, nums2, 0, 0)) / 2.0;
 		} else {
+			//For odd length of arry find the middle element
 			return findKth(total / 2 + 1, nums1, nums2, 0, 0);
 		}
 	}
