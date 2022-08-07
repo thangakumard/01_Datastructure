59136fe2f1357365ecef50c0fc48aef09482e783
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-08-06 23:15:21 -0700

    added Array40_SubarraySum

---
+++ algorithm/src/test/java/algorithms/array/medium/Array40_SubarraySum.java
@@ -0,0 +1,35 @@
+package algorithms.array.medium;
+
+import java.util.HashMap;
+
+/***
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ *
+ * Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+ *
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ *
+ * Example 1:
+ * Input: nums = [1,1,1], k = 2
+ * Output: 2
+ *
+ * Example 2:
+ * Input: nums = [1,2,3], k = 3
+ * Output: 2
+ */
+public class Array40_SubarraySum {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> mapSum = new HashMap<Integer, Integer>();
+        mapSum.put(0,1);
+        for(int i: nums){
+            sum += i;
+            if(mapSum.containsKey(sum-k)){
+                count += mapSum.get(sum-k);
+            }
+            mapSum.put(sum, mapSum.getOrDefault(sum, 0)+1);
+        }
+
+        return count;
+    }
+}
