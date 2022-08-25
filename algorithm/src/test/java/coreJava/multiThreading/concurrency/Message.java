8b20944a283cc408745e8bef6c1c730edb7216d4
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-08-25 16:44:24 -0700

    added concurrency sample

---
+++ algorithm/src/test/java/coreJava/multiThreading/concurrency/Message.java
@@ -0,0 +1,13 @@
+package coreJava.multiThreading.concurrency;
+
+public class Message {
+    private String msg;
+
+    public Message(String str){
+        this.msg=str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
