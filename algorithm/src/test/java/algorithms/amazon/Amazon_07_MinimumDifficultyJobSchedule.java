3f12e5852d9bdd11f3f9ce0386177e92888beedb
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-08-29 08:10:24 -0700

    Added few comments

--- algorithm/src/test/java/algorithms/amazon/Amazon_07_MinimumDifficultyJobSchedule.java
+++ algorithm/src/test/java/algorithms/amazon/Amazon_07_MinimumDifficultyJobSchedule.java
@@ -20,8 +20,10 @@ public class Amazon_07_MinimumDifficultyJobSchedule {
         int n = A.length, inf = Integer.MAX_VALUE, maxd;
         if (n < D) return -1;
         int[] dp = new int[n + 1];
+
         for (int i = n - 1; i >= 0; --i)
             dp[i] = Math.max(dp[i + 1], A[i]);
+
         for (int d = 2; d <= D; ++d) {
             for (int i = 0; i <= n - d; ++i) {
                 maxd = 0;
