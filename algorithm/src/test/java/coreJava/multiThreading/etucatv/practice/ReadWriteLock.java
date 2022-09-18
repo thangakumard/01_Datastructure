c2b4d672305706b7b45e7c8a4a80e78f8dc507f3
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-09-18 08:29:06 -0700

    MultiThread - ReadWriteLock

---
+++ algorithm/src/test/java/coreJava/multiThreading/etucatv/practice/ReadWriteLock.java
@@ -0,0 +1,31 @@
+package coreJava.multiThreading.etucatv.practice;
+
+
+public class ReadWriteLock {
+    int readers = 0;
+    boolean isWriteLock = false;
+
+    public synchronized void aquireReadLock() throws InterruptedException{
+        while(isWriteLock){
+            wait();
+        }
+        readers++;
+    }
+
+    public synchronized void releaseReadLock() {
+        readers--;
+        notify();
+    }
+
+    public synchronized void aquireWriteLock() throws InterruptedException{
+        while (readers > 0 || isWriteLock){
+            wait();
+        }
+        isWriteLock = true;
+    }
+
+    public synchronized void releaseWriteLock(){
+        isWriteLock = false;
+        notify();
+    }
+}
---
+++ algorithm/src/test/java/coreJava/multiThreading/etucatv/practice/ReadWriteTest.java
@@ -0,0 +1,61 @@
+package coreJava.multiThreading.etucatv.practice;
+
+public class ReadWriteTest {
+
+    public static void main(String[] args) throws Exception {
+        final ReadWriteLock rwl = new ReadWriteLock();
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Thread1 - Attempting to acquire write lock!");
+            try {
+                rwl.aquireWriteLock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread1 - Write lock is acquired");
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread1 - Releasing the write lock!");
+            rwl.releaseWriteLock();
+
+        });
+        Thread thread2 = new Thread(() -> {
+            System.out.println("Thread2 - Attempting to acquire write lock!");
+            try {
+                rwl.aquireWriteLock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread2 - Write lock is acquired");
+        });
+
+        Thread read1 = new Thread(() -> {
+            System.out.println("read1 - Attempting to acquire read lock!");
+            try {
+                rwl.aquireReadLock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread read2 = new Thread(() -> {
+            System.out.println("read2 - Releasing the read lock!");
+            rwl.releaseReadLock();
+        });
+
+        thread1.start();
+        thread2.start();
+        read1.start();
+        read2.start();
+    }
+
+}
