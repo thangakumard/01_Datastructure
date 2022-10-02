bde72102bc49ff00e57322729bdd426e8c597113
THANGAKUMAR DHANASEKARAN <dthangakumar@gmail.com>
2022-10-01 22:37:40 -0700

    CountingSemaphore is added

---
+++ algorithm/src/test/java/coreJava/multiThreading/etucatv/semaphore/CountingSemaphore.java
@@ -0,0 +1,73 @@
+package coreJava.multiThreading.etucatv.semaphore;
+
+class Demonstration {
+    public static void main( String args[] ) throws InterruptedException {
+
+        final CountingSemaphore cs = new CountingSemaphore(1);
+
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        cs.acquire();
+                        System.out.println("Ping " + i);
+                    }
+                } catch (InterruptedException ie) {
+
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        cs.release();
+                        System.out.println("Pong " + i);
+                    } catch (InterruptedException ie) {
+
+                    }
+                }
+            }
+        });
+
+        t2.start();
+        t1.start();
+        t1.join();
+        t2.join();
+    }
+}
+
+class CountingSemaphore {
+
+    int usedPermits = 0;
+    int maxCount;
+
+    public CountingSemaphore(int count) {
+        this.maxCount = count;
+
+    }
+
+    public synchronized void acquire() throws InterruptedException {
+
+        while (usedPermits == maxCount)
+            wait();
+
+        notify();
+        usedPermits++;
+
+    }
+
+    public synchronized void release() throws InterruptedException {
+
+        while (usedPermits == 0)
+            wait();
+
+        usedPermits--;
+        notify();
+    }
+}
