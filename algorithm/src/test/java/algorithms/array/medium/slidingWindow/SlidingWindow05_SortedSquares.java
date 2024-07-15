a5e252628efafd9080f529afd6bcd9d1bda338e7
Rubini Dhanasekaran <drubini90@gmail.com>
2024-07-15 07:19:38 -0700

    Added few new coding problems

---
+++ algorithm/src/test/java/algorithms/array/medium/slidingWindow/SlidingWindow05_SortedSquares.java
@@ -0,0 +1,28 @@
+package algorithms.array.medium.slidingWindow;
+
+/**
+ * Meta Interview Quest: Take the square of a sorted array
+ *
+ */
+public class SlidingWindow05_SortedSquares {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[pos] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[pos] = nums[right] * nums[right];
+                right--;
+            }
+            pos--;
+        }
+
+        return result;
+    }
+}
