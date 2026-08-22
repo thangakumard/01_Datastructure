902c5c5cc4fa9b57de55cf8255df50a15d5bf750
Thangakumar Dhanasekaran <dthangakumar@gmail.com>
2026-08-22 12:38:11 -0700

    rearranged array related solutions

---
+++ algorithm/src/test/java/algorithms/array/binarySearch/BinarySearch12_SearchInsert.java
@@ -0,0 +1,4 @@
+package java.algorithms.array.medium.binarySearch;
+
+public class BinarySearch12_SearchInsert {
+}
---
+++ algorithm/src/test/java/algorithms/hasset/Intersection.java
@@ -0,0 +1,4 @@
+package java.algorithms.hasset;
+
+public class Intersection {
+}
